0ac7ce626da799a7259e5e30dbb4c86656fa8853
Sheelendra <sheelendrasingh1904@gmail.com>
2025-03-20 22:19:24 +0530

    write some code to collect data from my pc

---
+++ data_collection.py
@@ -0,0 +1,16 @@
+import psutil
+import time
+
+def check_computer_health():
+    processor_busy = psutil.cpu_percent(interval=1)
+    memory_status = psutil.virtual_memory()
+    memory_used = memory_status.percent
+    running_tasks = []
+    for task in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent']):
+        running_tasks.append(task.info)
+    computer_stats = {
+        'processor_busy': processor_busy,
+        'memory_used': memory_used,
+        'running_tasks': running_tasks
+    }
+    return computer_stats
